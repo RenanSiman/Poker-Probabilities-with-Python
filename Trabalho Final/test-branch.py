f77a87a2b182cc11d6ff34e3d8e6769c4c425f56
RenanSiman <renanvinte2@hotmail.com>
2024-06-26 13:51:18 -0300

    test-homepage

---
+++ Trabalho Final/test-branch.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+# Define the HTML content for the home page
+home_page_html = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding: 50px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            color: #666;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to My Home Page</h1>
+    <p>This is a simple home page created using Flask.</p>
+</body>
+</html>
+'''
+
+@app.route('/')
+def home():
+    return render_template_string(home_page_html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
